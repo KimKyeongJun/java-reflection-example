afea349d1a7b8bbc56c9537225c8030011aa0148
Kyeongjun <v49011591@gmail.com>
2022-01-09 20:26:57 +0900

    리플렉션을 할 수 있는 다양한 방법

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws ClassNotFoundException {
+        // 클래스 타입을 가져오는 방법
+        //타입을 통해서 가져오는 방법
+        Class<Book> bookClass = Book.class;
+
+        // 인스턴스를 통해 가져오는 방법
+        Book boot = new Book();
+        Class<? extends Book> aClass = boot.getClass();
+
+        Class<?> aClass1 = Class.forName("org.example.Book");
+
+        // 클래스에 있는 모든 필드 출력
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(System.out::println);
+
+        Arrays.stream(bookClass.getDeclaredFields()).forEach(f -> {
+            try {
+                // 리플렉션에서는 접근 지시자를 무시할 수 있다.
+                f.setAccessible(true);
+                System.out.printf("%s %s\n", f, f.get(boot));
+            } catch (IllegalAccessException e) {
+                e.getStackTrace();
+            }
+        });
+
+        Arrays.stream(bookClass.getMethods()).forEach(System.out::println);
+
+        Arrays.stream(bookClass. getDeclaredConstructors()).forEach(System.out::println);
+
+        System.out.println(MyBook.class.getSuperclass());
+
+        Arrays.stream(MyBook.class.getInterfaces()).forEach(System.out::println);
+
+        Arrays.stream(Book.class.getFields()).forEach(f->{
+            int modifiers = f.getModifiers();
+            System.out.println(f);
+            System.out.println(Modifier.isPrivate(modifiers));
+            System.out.println(Modifier.isProtected(modifiers));
+            System.out.println(Modifier.isStatic(modifiers));
+        });
+
+        Arrays.stream(Book.class.getMethods()).forEach(m->{
+            int modifiers = m.getModifiers();
+            System.out.println(m);
+            System.out.println(Modifier.isInterface(modifiers));
+            System.out.println(Modifier.isProtected(modifiers));
+            System.out.println(Modifier.isStatic(modifiers));
+            System.out.println(m.getReturnType());
+            System.out.println(m.getParameterTypes());
+        });
+
+    }
+}
